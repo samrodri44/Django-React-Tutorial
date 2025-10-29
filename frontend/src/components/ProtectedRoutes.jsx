a25db8dd2d2d437740eb960170fa326afd6fcc7c
Samuel <samrodri44@gmail.com>
2025-10-29 13:17:25 -0400

    Finished ProjectedRoutes.jsx and stubbed basic pages .jsx files

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -2,15 +2,21 @@ import {Navigate} from "react-router-dom"
 import {jwtDecode} from "jwt-decode"
 import api from "../api"
 import { REFRESH_TOKEN, ACCESS_TOKEN } from "../constants"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function ProtectedRoute({children}) {
     const [isAuthorized, setIsAuthorized] = useState(null)
 
+    useEffect(() => {
+        auth().catch(() => {
+            setIsAuthorized(false)
+        })
+    })
+
     const refreshToken = async () => {
         const refreshToken = localStorage.getItem(REFRESH_TOKEN)
         try {
-            const res = api.post("/api/token/refresh/", {
+            const res = await api.post("/api/token/refresh/", {
                 refresh: refreshToken,
             }) 
             if (res.status === 200) {
